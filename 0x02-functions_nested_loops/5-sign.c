9076c1d8fa591b31d7fdb16e621ba7e78f6af8f7
amirabdullahi <amiremir346@gmail.com>
2023-02-21 13:34:51 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_sign - checks for alphabets
+ * @n: character to be checked
+ *
+ * Return: 1 if character is a letter, 0 otherwise
+ */
+	int print_sign(int n)
+	if (n > 0;)
+{
+	printf("+");
+	return (1);
+}
+	else if (n == 0)
+{
+	printf("0");
+	return (0);
+}
+	else
+{
+	printf("-")
+	return (-1);
+}
+}
