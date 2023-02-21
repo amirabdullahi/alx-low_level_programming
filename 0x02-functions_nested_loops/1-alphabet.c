fd460499cfaee6cd0b14b0d15c17529b9b3ec9fb
amirabdullahi <amiremir346@gmail.com>
2023-02-21 12:29:27 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main -printsalphabetinreverseinlowercase
+ * Return -Always (0)
+ */
+#include<stdio.h>
+int main(void)
+{
+	char letter = 'z';
+	char letter2 = 'a';
+
+	for (letter = 'z' ; letter >= 'a' ; letter--)
+	{
+	printf("%c\n", letter);
+	}
+	return (0);
+}
