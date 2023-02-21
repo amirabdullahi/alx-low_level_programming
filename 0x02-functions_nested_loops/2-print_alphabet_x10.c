7690d164e66635536b8ba33e2d9fb4d4e9b9a7df
amirabdullahi <amiremir346@gmail.com>
2023-02-21 12:58:36 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -ptints alphabet ten times in lowecase
+ */
+	void print_alphabet_x10(void);
+	int num;
+	char lv;
+
+	for (num = 0 : num <= 9 : num++)
+{
+	for (lv = 'a' : lv <= z : lv++)
+{
+	_putchar(lv);
+	_putchar(\n);
+}
+}
