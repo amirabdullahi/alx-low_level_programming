79287bed0f400161208c869863c03ea7d9819e7d
amirabdullahi <amiremir346@gmail.com>
2023-02-23 13:14:36 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,10 +8,16 @@ void print_most_numbers(void)
 {
 	int ch;
 
-	for (ch = 48; ch < 58; ch++)
+	for (ch = 48; ch <= 57; ch++)
 	{
-		if ((ch != 50) && (ch != 52))
+		if (ch == 50 || ch == 52)
+	{
+		continue;
+	}
+		else
+		{
 			_putchar(ch);
+	}
 	}
 	_putchar('\n');
 }
