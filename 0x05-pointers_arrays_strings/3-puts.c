20f7a496cd6dfbe7fb292ffc3bed28d197cfce95
amirabdullahi <amiremir346@gmail.com>
2023-02-28 16:17:48 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_puts - prints string
+ *
+ *@str: a string
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
