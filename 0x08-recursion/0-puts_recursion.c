0fbe681f1f9814bb11ff4af903f1c65d7784d58d
amirabdullahi <amiremir346@gmail.com>
2023-03-08 11:26:06 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion : prints characters of a string
+ * @s : character to be printed
+ * Return : no return
+ */
+	void _puts_recursion(char *s);
+	if (*s)
+	{
+		_putchar(s);
+	       _puts_recursion(s + 1);
+	}
+	else
+
+	_putchar('\n');
+	}
