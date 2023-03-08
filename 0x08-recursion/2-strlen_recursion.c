56293a847775712620d3c1045a84ae05851b2c8f
amirabdullahi <amiremir346@gmail.com>
2023-03-08 12:07:59 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strlen_recursion - returns the length of a sting
+ * _strlen_recursion - prints the length of a sting
  * @s : length to be counted
  * Return : string length of the string
  */
