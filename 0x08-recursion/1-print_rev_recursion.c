2a5a8d57f6e725dc949f712e423f47a10addcd6d
amirabdullahi <amiremir346@gmail.com>
2023-03-08 11:50:28 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints characters in reverse
+ * @s : character to be printed
+ * Return : No return
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+	_putchar(*s);
+	_print_rev_recursion(s - 1);
+	}
+	else
+	_putchar('\n');
+	}
