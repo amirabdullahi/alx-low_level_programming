769bbb340697779f9fb85d7648af2e999d621d66
amirabdullahi <amiremir346@gmail.com>
2023-03-10 14:11:57 -0800

    static libs

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,12 +12,12 @@ int main(int argc, char *argv[])
 
 	if (argc < 3)
 	{
-		printf("Error");
+		printf("Error\n");
 		return (1);
 	}
 	mult = atoi(argv[1]) * atoi(argv[2]);
 
-			printf("%d\n", mult);
+	printf("%d\n", mult);
 			return (0);
 }
 
