37bf3cb6c6ff572f8669f2aa4c0b051becb224f6
amirabdullahi <amiremir346@gmail.com>
2023-03-10 13:42:16 -0800

    static

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints number of arguments
+ * @argc: counts number of values
+ * @argv: array count
+ * Return: 0
+ */
+int main(int argc, char *argc[])
+{
+	printf("%d\n", argc - 1);
+	return (o);
+}
